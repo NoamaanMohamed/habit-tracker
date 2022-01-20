20fc377128d82fbc5002ef24e6d13b9d009f8a78
NoamaanMohamed <noamaan.mohamed@hotmail.co.uk>
2022-01-20 20:01:34 +0000

    lol

--- api/db_config/config.js
+++ api/db_config/config.js
@@ -1,5 +1,7 @@
 const Pool = require("pg").Pool;
-require("dotenv").config();const isProduction = process.env.NODE_ENV === "production";
+require("dotenv").config();
+
+const isProduction = process.env.NODE_ENV === "production";
 
 
 const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
